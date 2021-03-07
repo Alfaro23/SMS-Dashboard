94a83256cb621daf7deb13211751bd1c44a01eff
Alfaro23 <vterdohleb87@gmail.com>
2021-03-07 16:38:50 +0200

    update buttons

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,9 @@ let menu = document.querySelector(".mobile-nav");
 let login = document.querySelector(".buttons__login");
 let modalWindow = document.querySelector(".modal");
 let closeMenuLogin = document.querySelector(".closeMenuLogin");
+let email = document.querySelector("#email");
+let discover = document.querySelector(".discover__anchor"); 
+let anchor = document.querySelector(".trial__anchor");
 
 function openMobMenu(){
     menu.style.display = "block";
@@ -23,3 +26,29 @@ closeMenu.addEventListener("click", closeMobMenu);
 
 login.addEventListener("click", () => modalWindow.style.display = "flex");
 closeMenuLogin.addEventListener("click", () => modalWindow.style.display = "none");
+
+
+anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href');
+    email.focus();
+    console.log(blockID);
+    
+    document.querySelector(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+})
+
+discover.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = discover.getAttribute('href');
+    console.log(blockID);
+    
+    document.querySelector(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+})
