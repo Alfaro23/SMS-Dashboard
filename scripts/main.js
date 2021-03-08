46ce61672f4bc8701b27d53e487a811dfdadb907
Alfaro23 <vterdohleb87@gmail.com>
2021-03-08 17:19:34 +0200

    last fix

--- scripts/main.js
+++ scripts/main.js
@@ -7,19 +7,40 @@ let closeMenuLogin = document.querySelector(".closeMenuLogin");
 let email = document.querySelector("#email");
 let discover = document.querySelector(".discover__anchor"); 
 let anchor = document.querySelector(".trial__anchor");
+let header = document.querySelector(".header");
+let timerOpen;
+let timerClose;
 
 function openMobMenu(){
     menu.style.display = "block";
     openMenu.style.display = "none";
     closeMenu.style.display = "block";
+    clearInterval(timerOpen);
+
+    timerClose = setInterval(()=>{
+      if(header.clientWidth >= 754){
+        closeMenu.style.display = "none";
+        menu.style.display = "none";
+      } else{
+        closeMenu.style.display = "block";
+      }
+    }, 1000);
 }
 
 function closeMobMenu(){
     menu.style.display = "none";
     openMenu.style.display = "block";
     closeMenu.style.display = "none";
-}
+    clearInterval(timerClose);
 
+    timerOpen = setInterval(()=>{
+      if(header.clientWidth >= 754){
+        openMenu.style.display = "none";
+      } else{
+        openMenu.style.display = "block";
+      }
+    }, 1000);
+}
 
 openMenu.addEventListener("click", openMobMenu);
 closeMenu.addEventListener("click", closeMobMenu);
